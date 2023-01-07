467497fd5283bf04e3a0854a3cdb1cac8472aa9b
Zahidtz <121258605+Zahidtz@users.noreply.github.com>
2023-01-06 21:52:05 -0600

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,33 @@
+const int redPin = 9;
+const int bluePin = 10;
+const int greenPin = 11;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop() {
+  // Generate a random number between 0 and 99
+  int randNumber = random(100);
+
+  if (randNumber < 15) {
+    // 15% chance of landing on red
+    digitalWrite(redPin, HIGH);
+    delay(1000);
+    digitalWrite(redPin, LOW);
+  } else if (randNumber < 30) {
+    // 15% chance of landing on blue
+    digitalWrite(bluePin, HIGH);
+    delay(1000);
+    digitalWrite(bluePin, LOW);
+  } else {
+    // 70% chance of landing on green
+    digitalWrite(greenPin, HIGH);
+    delay(1000);
+    digitalWrite(greenPin, LOW);
+  }
+
+  delay(1000);
+}
